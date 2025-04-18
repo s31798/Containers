979834e362d00952cc90d1ff5a519cd0be81b991
POWER617 <148326539+POWER617@users.noreply.github.com>
2025-04-18 11:19:59 +0200

    database connection

--- src/Containers.Application/ContainerService.cs
+++ src/Containers.Application/ContainerService.cs
@@ -1,6 +1,48 @@
-﻿namespace Containers.Application;
+﻿
+using Containers.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Containers.Application;
 
 public class ContainerService
 {
-    
+    private string _connectionString;
+    public ContainerService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    public IEnumerable<Container> Containers()
+    {
+        List<Container> containers = [];
+        const string quaryString = "SELECT * FROM Containers";
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(quaryString, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            try
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var containerRow = new Container
+                        {
+                            ID = reader.GetInt32(0),
+                            ContainerTypeId = reader.GetInt32(1),
+                            IsHazardious = reader.GetBoolean(2),
+                            Name = reader.GetString(3),
+                        };
+                        containers.Add(containerRow);
+                    }
+                }
+
+            }
+            finally
+            {
+               reader.Close();
+            }
+        }
+        return containers;
+    }
 }
--- src/Containers.Models/Container.cs
+++ src/Containers.Models/Container.cs
@@ -2,5 +2,8 @@
 
 public class Container
 {
-    
+    public int ID{get;set;}
+    public int ContainerTypeId{get;set;}
+    public bool IsHazardious{get;set;}
+    public string Name{get;set;}
 }
